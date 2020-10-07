9cb77b897c7d2511f266bd7dfa222b5568a1dd71
Sergei Cherniaev <sergei@cherniaev.com>
2020-10-07 15:11:18 +0300

    fix(build): add import to fix build

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 import { GA_TRACKING_ID } from "../lib/gtag";
 
