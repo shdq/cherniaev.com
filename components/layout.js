0552bf7be06d38895e45c9ee7072688de11dce24
Sergei Cherniaev <sergei@cherniaev.com>
2023-05-11 16:45:09 +0400

    feat(layout): update header layout of blog post page

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import { Heading, Text, useTheme } from "spartak-ui";
+import { Heading, Text, Avatar, useTheme } from "spartak-ui";
 import { Toggle } from "./toggle";
 import { inverseSvg, reduceImageBrightness } from "../themes/helpers";
 
@@ -27,43 +27,46 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.flexRight}>
-        <Toggle />
-      </div>
-      <header className={styles.header}>
+      <>
         {home ? (
           <>
-            <Image
-              src={profilePic}
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-              placeholder="blur"
-            />
-            <Heading as="h1" size="xxl" css={{ padding: "$paddingLG 0" }}>
-              {name}
-            </Heading>
-          </>
-        ) : (
-          <>
-            <Link href="/">
+            <div className={styles.flexRight}>
+              <Toggle />
+            </div>
+            <header className={styles.header}>
               <Image
                 src={profilePic}
-                className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
                 alt={name}
                 placeholder="blur"
               />
-            </Link>
-            <Heading as="p" size="xl" css={{ padding: "$paddingLG 0" }}>
-              <Link
-                href="/"
-                className={`${utilStyles.colorInherit} ${utilStyles.blogLink}`}
-              >
+              <Heading as="h1" size="xxl" css={{ padding: "$paddingLG 0" }}>
                 {name}
-              </Link>
-            </Heading>
+              </Heading>
+            </header>
           </>
+        ) : (
+          <div className={styles.spaceBetween}>
+            <header className={styles.postHeader}>
+              <Link href="/">
+                <Avatar round size="md" src={profilePic.src} alt={name} />
+              </Link>
+              <Heading as="p" size="lg" css={{ padding: "$paddingLG 0" }}>
+                <Link
+                  href="/"
+                  className={`${utilStyles.colorInherit} ${utilStyles.blogLink}`}
+                >
+                  {name}{" "}
+                  <Text as="span" secondary>
+                    Blog
+                  </Text>
+                </Link>
+              </Heading>
+            </header>
+            <Toggle />
+          </div>
         )}
-      </header>
+      </>
       <main>{children}</main>
       {!home && (
         <Text as={Link} href="/" size="md" color="red">
